1768602cee0a70eef669e9b51c7bc208ff35a76f
chepkoy <allankiplangat22@gmail.com>
2017-12-03 17:57:57 +0300

    Python relational operators

---
+++ relational.py
@@ -0,0 +1,15 @@
+# Relational Operators
+
+# Value equality / equivalence
+g = 20
+print(g == 20, g == 13)
+
+# Value inequality / inequivalence
+print(g != 20, g != 13)
+# Less than and greater than
+
+print(g < 30, g > 30)
+
+# Less-than or equal to Greater-than or equal to
+
+print(g <= 20, g>= 20)

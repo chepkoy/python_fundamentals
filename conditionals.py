2f16e2ce5a5ad704fc42d3aac94725e4c8929986
chepkoy <allankiplangat22@gmail.com>
2017-12-03 20:00:35 +0300

    python conditional statements

---
+++ conditionals.py
@@ -0,0 +1,25 @@
+# Python conditional statements
+
+if True:
+    print("Its true")
+
+if False:
+    print("Its False")
+
+if bool("eggs"):
+    print("yes please!")
+
+if "eggs":
+    print("Yes Please!")
+
+# Else Clause
+
+h = 42
+if h > 50:
+    print("Greater than 50")
+
+elif h < 20:
+    print("Less than 20")
+
+else:
+    print("Between 20 and 50 ")

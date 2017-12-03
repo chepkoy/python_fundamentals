5e5ca89eeaccbd7abf5ec2d8f42943683fab6217
chepkoy <allankiplangat22@gmail.com>
2017-12-04 02:22:26 +0300

    for loops and keeping it all together

---
+++ for_loop.py
@@ -0,0 +1,10 @@
+# Visit each item in an iterable series
+cities = ['nairobi', 'mombasa', 'kisumu']
+
+for city in cities:
+    print(city)
+
+colors = {'crimson': 0xdc143c, 'coral': 0xff7f50}
+
+for color in colors:
+    print(color, colors[color])

5e5bcd1a6cf0d169cb250502cae60bb1758e7bd9
chepkoy <allankiplangat22@gmail.com>
2017-12-05 00:10:37 +0300

    Extended the word module

--- words.py
+++ words.py
@@ -1,9 +1,26 @@
+#!/usr/bin/env python3
+""" Retrieve and print words from a URL.
+
+Usage:
+    python 3 words.py <URL>
+
+"""
 import sys
 from urllib.request import urlopen
 
 
 # python construct with block
 def fetch_words(url):
+    """Fetch a list of words from a URL.
+
+    Args:
+        url: The Url of a UTF-* text document.
+
+    Returns:
+        A list of strings containing the words from the
+        the document.
+
+    """
     with urlopen(url) as story:
         story_words = []
         for line in story:
@@ -14,15 +31,25 @@ def fetch_words(url):
 
 
 def print_items(items):
+    """Prints items one per line.
+
+    Args:
+        An iterable series of printable items.
+    """
     for item in items:
         print(item)
 
 
 # Setting up a main() function with a command line argument
 def main(url):
+    """Prints each word from a text document from a URL
+
+    Args:
+        url: The Url of UTF-8 text document.
+    """
     words = fetch_words(url)
     print_items(words)
 
 
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main(sys.argv[1])  # The 0th arg is the module filename

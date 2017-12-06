16396dafc51fc62e309d5a06dd5203c77c6b5b60
chepkoy <allankiplangat22@gmail.com>
2017-12-07 01:29:40 +0300

    Finished lst collection

--- list_collection.py
+++ list_collection.py
@@ -43,4 +43,77 @@ copy = texts.copy()
 
 listed = list(texts)
 
-print(listed)
+# Repeating lists using the * operator
+# Mostly used for initializing a list of known size with a conststant
+# Multiple refs to one instance of the constant in product list
+
+repeat = [21, 37]
+product = repeat * 4
+
+repeat_two = [0] * 9
+
+# Finding Elements
+# ValueError if not found
+
+w = "the quick brown fox jumps over the lazy dog".split()
+i = w.index('fox')
+
+# count returns the number of matching elements
+
+match = w.count('the')
+
+# The in and not in operators test for membership
+
+test = 37 in [1, 44, 54, 634, 37]
+test_two = 37 not in [1, 44, 54, 634, 37]
+
+# del [index] to remove by index
+
+u = "jackdaws love my big sphinx of quartz".split()
+del u[3]
+
+# remove to remove by value raises ValueError if not present
+u.remove('jackdaws')
+
+# remove  equivalent to
+#del seq[seq.index(item)]
+
+# inserting elements to a list
+
+a = "i accidentally the whole universe".split()
+two = a.insert(2, "destroyed")
+
+# Concatenation with the + operator
+
+m = [2, 1, 3]
+n = [4, 7, 11]
+
+k = m + n
+
+# in -place extension with += operator or extend() method
+
+k += [18, 29, 47]
+k.extend([23, 45, 65, 8])
+
+# Reverse in place
+
+g = [1, 11, 21, 1211]
+g.reverse()
+
+# sorting using the sort method
+g.sort()
+g.sort(reverse=True)
+
+# key argument to sort() method accepts a function for producing a sort key from an item
+
+h = " not surprising at all"
+h.sort(key=len)
+
+# using the sorted function
+x = [4, 9, 2, 1]
+y = sorted(x)
+
+# using the reverse function returns an iterator
+
+p = [9, 3, 1, 0]
+q = reversed(p)
